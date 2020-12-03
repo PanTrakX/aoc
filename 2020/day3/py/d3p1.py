fc92b2b0ff381eabb7a4f1f76ea62528fd8a7417
Panagiotis Trakadas <pantrakx@protonmail.com>
2020-12-03 17:40:31 +0200

    Day 3

---
+++ 2020/day3/py/d3p1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    geology = []
+    current_pos = [0, 0]
+    trees_encountered = 0
+    x_length = 0
+
+    with open("../input.txt", "r") as f:
+        geology = [line.strip() for line in f]
+
+    x_length = len(geology[0])
+    y_length = len(geology)
+
+    while True:
+        current_pos[0] = current_pos[0] + 1
+        current_pos[1] = (current_pos[1] + 3) % x_length
+        if current_pos[0] < y_length:
+            if geology[current_pos[0]][current_pos[1]] == "#":
+                trees_encountered += 1
+        else:
+            print(trees_encountered)
+            return 0
+
+
+if __name__ == "__main__":
+    exit(main())
